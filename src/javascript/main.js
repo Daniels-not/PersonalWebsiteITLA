9347db1ba3a851e416ca37545907709d2421883b
Daniels-not <ramydcampusano@hotmail.com>
2021-09-12 01:05:39 -0400

    PERSONAL WEBSITE ITLA

---
+++ src/javascript/main.js
@@ -0,0 +1,21 @@
+// SCROOL REVEAL JS CONFIGURATION
+
+window.sr = ScrollReveal();
+
+    sr.reveal('#about', {
+        duration: 1000,
+        origin: 'bottom',
+        distance: '-100px'
+    })
+
+    sr.reveal('#hobbies', {
+        duration: 3000,
+        origin: 'left',
+        distance: '-100px'
+    })
+
+    sr.reveal('#one__video', {
+        duration: 3000,
+        origin: 'right',
+        distance: '-100px'
+    })
